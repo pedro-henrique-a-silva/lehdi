84312ba76e2997a743170f92db2fae615ce8b6cf
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-06-15 20:32:04 -0300

    Efeito do teclado somente nas letras

--- src/script.js
+++ src/script.js
@@ -1,7 +1,21 @@
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', (event) => {
   // Captura o código da tecla pressionada
-  const keyCode = event.keyCode || event.which;
-
+  const keyCode = event.key;
+  const tecla = document.querySelector(`#${keyCode}`);
+  if (tecla) {
+    tecla.classList.toggle('active')
+  }
   // Exibe o código da tecla no console
   console.log('Tecla pressionada:', keyCode);
+});
+
+document.addEventListener('keyup', (event) => {
+  // Captura o código da tecla pressionada
+  const keyCode = event.key;
+  const tecla = document.querySelector(`#${keyCode}`);
+  if (tecla) {
+    tecla.classList.toggle('active')
+  }
+  // Exibe o código da tecla no console
+  console.log('Tecla Solta:', keyCode);
 });
