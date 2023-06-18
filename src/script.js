3a24e80873ba93e8ee8f120268d4366022b9fa9b
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-06-18 13:11:24 -0300

    Capturando evento teclas do alfabeto

--- src/script.js
+++ src/script.js
@@ -1,18 +1,18 @@
 document.addEventListener('keydown', (event) => {
   // Captura o código da tecla pressionada
   const keyCode = event.key;
-  const tecla = document.querySelector(`#${keyCode}`);
+  console.log('Tecla pressionada:', keyCode);
+  const tecla = document.querySelector(`#tecla-${keyCode}`);
   if (tecla) {
     tecla.classList.toggle('active')
   }
   // Exibe o código da tecla no console
-  console.log('Tecla pressionada:', keyCode);
 });
 
 document.addEventListener('keyup', (event) => {
   // Captura o código da tecla pressionada
   const keyCode = event.key;
-  const tecla = document.querySelector(`#${keyCode}`);
+  const tecla = document.querySelector(`#tecla-${keyCode}`);
   if (tecla) {
     tecla.classList.toggle('active')
   }
