0539c3de274f9ab48e8be763b067d77dedde6cb9
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-06-25 13:06:52 -0300

    Criando ambiente de dev com npm e vite e modo import ES6

--- src/script.js
+++ src/script.js
@@ -1,10 +1,16 @@
+import './style/keyboard.css';
+import './style/letterBoard.css';
+import './style/navPhase.css';
+import './style/statusTyping.css';
+import './style/style.css';
+
 document.addEventListener('keydown', (event) => {
   // Captura o código da tecla pressionada
   const keyCode = event.key;
   console.log('Tecla pressionada:', keyCode);
   const tecla = document.querySelector(`#tecla-${keyCode}`);
   if (tecla) {
-    tecla.classList.toggle('active')
+    tecla.classList.toggle('active');
   }
   // Exibe o código da tecla no console
 });
@@ -14,7 +20,7 @@ document.addEventListener('keyup', (event) => {
   const keyCode = event.key;
   const tecla = document.querySelector(`#tecla-${keyCode}`);
   if (tecla) {
-    tecla.classList.toggle('active')
+    tecla.classList.toggle('active');
   }
   // Exibe o código da tecla no console
   console.log('Tecla Solta:', keyCode);
